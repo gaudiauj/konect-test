ee646a7de47928f37ff7e9bd0d97e65ced60b43a
Jean Gaudiau <jean@react-formation.fr>
2022-07-02 22:20:07 +0200

    cleanup default code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useReducer, useRef } from "react";
+import css from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <>
+      <header className={`${css.header}`}>
+        <h1>Konect test</h1>
       </header>
-    </div>
-  )
+      <form>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+        <input type="text" pattern="[0-9]" placeholder="."></input>
+      </form>
+    </>
+  );
 }
 
-export default App
+export default App;
